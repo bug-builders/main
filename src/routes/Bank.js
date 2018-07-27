af7c119ff1874afb34ad48298997b7badd8a0b55
agix <florian.gaultier@gmail.com>
2018-07-27 18:28:23 +0200

    Fallback to real label

--- src/routes/Bank.js
+++ src/routes/Bank.js
@@ -77,6 +77,9 @@ export default () => {
             }
             if(note) {
               label = note;
+              if(typeof(label.label) === 'undefined'){
+                label.label = transaction.label;
+              }
             } else {
               ({label} = transaction)
             }
